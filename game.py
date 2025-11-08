ddd7619447d6c6b7a2b28961c7a4953f5ebf28f0
Nano <nanoticity@gmail.com>
2025-11-08 09:08:12 -0800

    Format

--- game.py
+++ game.py
@@ -1,36 +1,43 @@
 import pygame as pg
 
+
 class Brick:
     def __init__(self, x, y):
         self.pos = (x, y)
         self.dimensions = (76, 20)
-        self.rect = pg.Rect(self.pos[0], self.pos[1], self.dimensions[0], self.dimensions[1])
-        
+        self.rect = pg.Rect(
+            self.pos[0], self.pos[1], self.dimensions[0], self.dimensions[1]
+        )
+
     def draw(self, surface):
         pg.draw.rect(surface, pg.color.Color("black"), self.rect)
 
     def is_collide(self, ball) -> bool:
-        ball_rect = pg.Rect(ball.x - ball.radius, ball.y - ball.radius, ball.radius * 2, ball.radius * 2)
+        ball_rect = pg.Rect(
+            ball.x - ball.radius, ball.y - ball.radius, ball.radius * 2, ball.radius * 2
+        )
         is_collide = self.rect.colliderect(ball_rect)
         if is_collide:
             ball.y_add *= -1
         return is_collide
 
+
 class BrickManager:
     def __init__(self):
         self.bricks = []
-    
+
     def create_bricks(self):
         for y in range(5):
             for x in range(10):
-                self.bricks.append(Brick(x*80+2, y*24+2))
-    
+                self.bricks.append(Brick(x * 80 + 2, y * 24 + 2))
+
     def draw_bricks(self, surface, ball):
         for brick in self.bricks:
             if brick.is_collide(ball):
                 self.bricks.remove(brick)
                 continue
             brick.draw(surface)
+
 
 class Ball:
     def __init__(self, x, y):
@@ -45,16 +52,16 @@ class Ball:
     def move(self, paddle):
         self.x += self.x_add
         self.y += self.y_add
-        
+
         self.collisions_handler(paddle)
-        
+
     def collisions_handler(self, paddle):
         if self.x <= 0 + self.radius or self.x >= 800 - self.radius:
             self.x_add *= -1
-        
+
         if self.y <= 0 + self.radius:
             self.y_add *= -1
-        
+
         if not self.hit:
             if self.y >= 760 - self.radius and self.y <= 780 - self.radius:
                 if self.x >= paddle.x and self.x <= paddle.end_x:
@@ -64,13 +71,13 @@ class Ball:
                     self.x_add = distance_from_center / 5
                     self.y_add *= -1
                     self.hit = True
-        
+
         if self.y <= 400:
             self.hit = False
-            
+
         if self.y >= 800 + self.radius:
             self.reset()
-            
+
     def reset(self):
         pg.time.wait(100)
         self.x = self.initial_values[0]
@@ -80,6 +87,7 @@ class Ball:
 
     def draw(self, surface):
         pg.draw.circle(surface, pg.color.Color("black"), (self.x, self.y), self.radius)
+
 
 class Paddle:
     def __init__(self, x, y):
@@ -103,12 +111,13 @@ class Paddle:
             self.x = max_x
 
         self.position_update()
-            
+
     def position_update(self):
         self.middle_x = self.x + self.width / 2
         self.end_x = self.x + self.width
         print(self.end_x)
 
     def draw(self, surface):
-        pg.draw.line(surface, pg.color.Color("black"), (self.x, self.y), (self.end_x, self.y), 20)
-
+        pg.draw.line(
+            surface, pg.color.Color("black"), (self.x, self.y), (self.end_x, self.y), 20
+        )
--- main.py
+++ main.py
@@ -3,16 +3,17 @@ from game import *
 
 pg.init()
 
+
 def main():
     screen = pg.display.set_mode([800, 800])
-    
+
     brick_manager = BrickManager()
     brick_manager.create_bricks()
-    
+
     paddle = Paddle(350, 760)
-    
+
     ball = Ball(300, 500)
-    
+
     clock = pg.time.Clock()
 
     run = True
@@ -20,13 +21,13 @@ def main():
         for e in pg.event.get():
             if e.type == pg.QUIT:
                 run = False
-                
+
         screen.fill(pg.color.Color("white"))
         brick_manager.draw_bricks(screen, ball)
-        
+
         paddle.draw(screen)
         paddle.move()
-        
+
         ball.draw(screen)
         ball.move(paddle)
 
